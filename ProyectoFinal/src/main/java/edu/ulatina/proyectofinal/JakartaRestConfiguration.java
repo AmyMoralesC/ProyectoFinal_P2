8b503ab393bdf7696a7df9a022c8cd4ef04955e1
AmyMoralesC <80178519+AmyMoralesC@users.noreply.github.com>
2024-06-27 00:27:46 -0600

    Subiendo el archivo principal
    Agregué solamente el index.xhtml

---
+++ ProyectoFinal/src/main/java/edu/ulatina/proyectofinal/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package edu.ulatina.proyectofinal;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}

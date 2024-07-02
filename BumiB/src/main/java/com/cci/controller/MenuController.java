e33cd309973bff182f3581e30cb2e2f6a9f2d275
AmyMoralesC <amysmc@outlook.com>
2024-07-01 21:52:16 -0600

    Avance de la página principal y menú - (sigue en proceso)

---
+++ BumiB/src/main/java/com/cci/controller/MenuController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cci.controller;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Amy
+ */
+@ManagedBean(name = "menuController")
+@SessionScoped
+public class MenuController implements Serializable {
+    
+    
+}

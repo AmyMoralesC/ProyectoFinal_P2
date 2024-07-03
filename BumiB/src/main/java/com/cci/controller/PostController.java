11a8c883eaf3c6781f52c36445291f3d20e7f3f1
jared <jared@DESKTOP-FDV3I38>
2024-07-03 16:03:58 -0600

    Se elimino algunos errores importantes

---
+++ BumiB/src/main/java/com/cci/controller/PostController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cci.controller;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author jared
+ */
+public class PostController implements Serializable{
+
+    
+    public void crearpost(){
+        
+    }
+    
+    public void mostrarpost() {
+        
+    }
+    
+    
+}
---
+++ BumiB/src/main/java/com/cci/model/Post.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cci.model;
+
+/**
+ *
+ * @author jared
+ */
+public class Post {
+
+    private int Id;
+    private String titulo;
+    private String texto;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}

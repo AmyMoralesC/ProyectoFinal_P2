2a5092df5c98837b1fe8caac2db4088345a64d21
vladr <vladr@DESKTOP-QK88J6M>
2024-07-24 02:44:30 -0600

    Parte de las publicaciones, fecha, Publicacion ectendida etc

--- BumiB/src/main/java/com/cci/controller/PostController.java
+++ BumiB/src/main/java/com/cci/controller/PostController.java
@@ -3,12 +3,15 @@ package com.cci.controller;
 import com.cci.data.ServicioPost;
 import com.cci.model.Post;
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
@@ -31,21 +34,24 @@ public class PostController implements Serializable {
     }
 
     public void crearPost() {
-        post.setFecha(new Date());
+        post.setFecha(new Timestamp(System.currentTimeMillis()));
         post.setTexto(nuevoTexto);
-        post.setNotifi(0);
         boolean exito = servicioPost.crearPost(post);
         if (exito) {
             posts.add(post);
             post = new Post();
             nuevoTexto = "";
-            post.setNotifi(1);
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Publicación creada exitosamente"));
-            
         } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No se pudo crear la publicación"));
-            
         }
+    }
+
+    // Método para formatear la fecha en español
+    public String formatDate(Timestamp timestamp) {
+        Date date = new Date(timestamp.getTime());
+        DateFormat df = new SimpleDateFormat("dd MMMM yyyy", new Locale("es", "ES"));
+        return df.format(date);
     }
 
     // Getters y Setters
